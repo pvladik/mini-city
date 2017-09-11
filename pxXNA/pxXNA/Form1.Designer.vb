21dcea2ebf60ca104e4730504f0100583926e4ed
pvladik <pvladik@centrum.cz>
2017-09-11 10:20:18 +0200

    f
    f

---
+++ pxXNA/pxXNA/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tb_debug = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'tb_debug
+        '
+        Me.tb_debug.Location = New System.Drawing.Point(12, 12)
+        Me.tb_debug.Multiline = True
+        Me.tb_debug.Name = "tb_debug"
+        Me.tb_debug.Size = New System.Drawing.Size(572, 351)
+        Me.tb_debug.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(587, 368)
+        Me.Controls.Add(Me.tb_debug)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tb_debug As Windows.Forms.TextBox
+End Class
