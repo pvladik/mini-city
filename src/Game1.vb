488382f98cfce5088a3a3438783416dc8a90eb0c
clarje83 <j.clarke2003@outlook.ie>
2022-01-19 08:35:16 +0000

    making sure imports work

---
+++ src/Game1.vb
@@ -0,0 +1,7 @@
+﻿Imports Microsoft.Xna.Framework
+Imports Microsoft.Xna.Framework.Input
+Imports Microsoft.Xna.Framework.Graphics
+
+Public Class Game1
+    Inherits Microsoft.Xna.Framework.Game
+End Class
